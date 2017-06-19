151f157cb13882d45bdcde41e13c9d82a2d3393e
Pierre Tusseau <pierre.tusseau@gmail.com>
2017-06-19 13:32:04 +0200

    Ajout page category & contact

---
+++ category.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+
+<div id="single-post" role="main">
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+		<header>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+		</div>
+		<footer>
+			<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+			<p><?php the_tags(); ?></p>
+		</footer>
+	</article>
+<?php endwhile;?>
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+<?php //get_sidebar(); ?>
+</div>
+<?php get_footer();
