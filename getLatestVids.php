cbe15e8da87ee759fa34e68d7c8567a54eba6b9f
Kevin Bacas <kevinbacas@mbp-de-kevin.home>
2017-08-01 20:26:36 +0200

    Now uses curl for request

--- getLatestVids.php
+++ getLatestVids.php
@@ -11,7 +11,26 @@ function getlatestvids() {
   $channelId = get_field('home-youtube-channel', HOME_PAGE_ID);
   $apiKey = get_field('home-youtube-api', HOME_PAGE_ID);
   $url = "https://www.googleapis.com/youtube/v3/search?key=$apiKey&channelId=$channelId&part=snippet,id&order=date&maxResults=$count";
-  $data = file_get_contents($url, false);
-  $clean_data = substr($data, 0, strrpos($data, "\n"));
-  return json_decode($clean_data);
+
+  $curl = curl_init();
+  if (DEBUG_CURL) {
+    curl_setopt($curl, CURLOPT_VERBOSE, true);
+    $verbose = fopen('php://temp', 'w+');
+    curl_setopt($curl, CURLOPT_STDERR, $verbose);
+  }
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER , TRUE);
+  $res = curl_exec($curl);
+  if (DEBUG_CURL) {
+    if ($result === FALSE) {
+        printf("cUrl error (#%d): %s<br>\n", curl_errno($curl),
+              htmlspecialchars(curl_error($curl)));
+    }
+    rewind($verbose);
+    $verboseLog = stream_get_contents($verbose);
+    echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+  }
+  curl_close($curl);
+  return json_decode($res);
 }
