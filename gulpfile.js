b787f4671e38b19ea2486b8f08bc1d3376ea53dc
Pierre Tusseau <pierre.tusseau@gmail.com>
2017-12-16 16:08:48 +0100

    Cacher les erreurs de lint js pendant le build

--- gulpfile.js
+++ gulpfile.js
@@ -143,10 +143,10 @@ gulp.task('lint', function() {
 
       var errors = file.jshint.results.map(function (data) {
         if (data.error) {
-          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+          // return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
         }
       }).join("\n");
-      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+      // return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
     }));
 });
 
